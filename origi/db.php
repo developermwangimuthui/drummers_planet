ad0c72f835c4b2b82c64f5a8b625c4b65f269bc5
Noblepal <zeronillzero@gmail.com>
2020-08-31 19:22:31 +0300

    mpesa

---
+++ origi/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database configuration
+ */
+
+define('DB_USERNAME', 'rennyexp_pesa');
+define('DB_PASSWORD', 'gS4hisiW[WM~');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'rennyexp_pesa');
+
+
+
+
+
+?>
